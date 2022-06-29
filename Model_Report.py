602eb7803a9460a52d933688245d2e90ce02f893
Yash Chauhan <68198489+ITrustNumbers@users.noreply.github.com>
2022-06-29 13:33:10 +0530

    Update Model_Report.py

--- Model_Report.py
+++ Model_Report.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.metrics import ConfusionMatrixDisplay, classification_report, RocCurveDisplay
 
 def model_report(clf, y, y_preds, labels, test_data=False, conf_matrix=False, class_report=False, roc_curve=False):
@@ -9,13 +11,11 @@ def model_report(clf, y, y_preds, labels, test_data=False, conf_matrix=False, cl
   1. Plot Confusion Martix
   2. Print Classification Report
   3. Plot ROC Curve
-
   In any combination, by default the function does nothing but the functionality can
   be turned on by simply passing arguments for:
   1. conf_matrix: True for plotting confusion matrix
   2. class_report: True for printing classification report
   3. ROC_Curve: True for plotting ROC Cruve
-
   User can specify whether to use training data or test data by:
   test_data: True for using test data & False for using training data
   '''
@@ -24,16 +24,20 @@ def model_report(clf, y, y_preds, labels, test_data=False, conf_matrix=False, cl
 
   #Plotting Confusion Matrix 
   if conf_matrix:
+    conf = confusion_matrix(y,y_preds)
     fig = plt.figure(figsize=(12,8))
-    ConfusionMatrixDisplay.from_predictions(y, y_preds, display_labels=labels, cmap='BuPu', 
-                                            ax=plt.gca(), colorbar=False)
+    sns.heatmap(conf, cmap='BuPu',  cbar=False, annot=True, fmt='d', annot_kws={'fontsize' : 14, 'weight' : 'bold'})
 
-    plt.tick_params(axis='both', pad=4, labelsize='large')
-    plt.title(f'Confusion Matrix for {clf_name}',fontsize=15)
-    plt.ylabel('True Label', fontsize=15)
-    plt.xlabel('Predicted Label', fontsize=15)
-    plt.show();
-    print('\n\n')
+    plt.tick_params(axis='both', pad=4, labelsize=16, size=0)
+
+    plt.gca().set_xticklabels(labels)
+    plt.gca().set_yticklabels(labels)
+
+    plt.title('Confusion Matrix for {}'.format(clf_name), 
+                  fontsize=17, pad=14)
+
+    plt.ylabel('True Label', fontsize=16, labelpad=14)
+    plt.xlabel('Predicted Label', fontsize=16, labelpad=14)
   
   #Classification report
   if class_report:
